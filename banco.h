e4f0fb23dea7926cda84812b3f88baf5cb20a505
kauane menezes <160642187+akauzinha47@users.noreply.github.com>
2024-03-27 17:13:46 -0300

    Aula 07
    Finalizando os exercícios da aula passada.

--- banco.h
+++ banco.h
@@ -1,9 +1,11 @@
-#include <stdio.h>
-
 #ifndef BANCO_H
 #define BANCO_H
 
+#include <stdio.h>
+#include <time.h>
+
 int money = 50, withdraw_qtd = 0, deposit_qtd = 0;
+
 char name[20] = "";
 
 FILE * arquivo;
@@ -13,23 +15,28 @@ void innit ()
     arquivo = fopen("extrato_e_movimentacoes.txt", "a");
     printf("\nDigite o nome do(a) dono(a) da conta: ");
     scanf("%s", name);
-    fprintf(arquivo, "\n == EXTRATO E MOVIMENTACOES DO(A) %s ==\n", name);
+    fprintf(arquivo, "== EXTRATO E MOVIMENTACOES DO(A) %s ==\n", name);
+    fclose(arquivo);
 }
 
 int deposit()
 {
-    printf("-- REALIZANDO DEPÓSITO --\n Quanto deseja depositar? ");
-    scanf("%d", deposit_qtd);
+    arquivo = fopen("extrato_e_movimentacoes.txt", "a");
+    printf("\n-- REALIZANDO DEPÓSITO --\nQuanto deseja depositar? ");
+    scanf("%d", &deposit_qtd);
     money = money + deposit_qtd;
-    printf("\nDEPÓSITO REALIZADO COM SUCESSO!");
+    printf("\n\nDEPÓSITO REALIZADO COM SUCESSO!");
     fprintf(arquivo, "\nDEPÓSITO DE: R$ %d", deposit_qtd);
+    fclose(arquivo);
+
+    return 0;
     
 }
 
 int withdraw()
 {
     printf("-- REALIZANDO SAQUE --\n Quanto deseja sacar? ");
-    scanf("%d", withdraw_qtd);
+    scanf("%d", &withdraw_qtd);
     if (withdraw_qtd > 100)
     {
         printf("\nA quantia que deseja sacar supera o limite estabelecido para a conta. :(\n");
@@ -40,10 +47,13 @@ int withdraw()
     }
     else
     {
+        arquivo = fopen("extrato_e_movimentacoes.txt", "a");
         money = money - withdraw_qtd;
         printf("\nSAQUE REALIZADO COM SUCESSO!");
-        fprintf(arquivo, "\nSAQUE DE: R$ %d", deposit_qtd);
+        fprintf(arquivo, "\n\nSAQUE DE: R$ %d", deposit_qtd);
+        fclose(arquivo);
     }
-}
 
+    return 0;
+}
 #endif
--- main.c
+++ main.c
@@ -1,6 +1,46 @@
-#include <stdio.h>
+#include "banco.h"
 
-int main()
+int main ()
 {
+    int ans = 0;
+
+    innit();
+
+    while (1)
+    {
+        
+        printf("\nQual operação deseja realizar agora?\n\n[1] Realizar depósito\n\n[2] Realizar saque\n\n[3] Sair do programa\n\nR: ");
+        scanf("%d", &ans);
+
+        switch (ans)
+        {
+        case 1:
+            deposit();
+            break;
+        
+        case 2:
+            withdraw();
+            break;
+
+        case 3:
+            break;
+
+        default:
+            printf("\nAAAAAAAAAAAAAAA");
+            break;
+        }
+
+    if (ans == 3)
+    {
+        printf("\nPrograma encerrado.");
+        break;
+    }
+    else
+    {
+        ;
+    }
     
+    }
+
+    return 0;
 }
