e372be74aecbf508520c845ceb5433f27cf6e880
akauzinha47 <kau.vmenezes@gmail.com>
2024-05-08 16:51:20 -0300

    feat: struct challange

---
+++ Structs - Agenda Challange/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char name[20];
+    char email[20];
+    char adress[20];
+    char phone[10];
+    char birth_date[11];
+} Contact;
+
+int main() {
+    return 0;
+}
