c9c7f7515c8019442a0b6a5702dba7aafa1afd23
akauzinha47 <kau.vmenezes@gmail.com>
2024-05-09 15:56:35 -0300

    feat: sorted push

--- Structs - Agenda Challange/add_contact.h
+++ Structs - Agenda Challange/add_contact.h
@@ -1,14 +1,18 @@
-#ifndef ADD_CONTACT.h
-#define ADD_CONTACT.h
+#ifndef ADD_CONTACT
+#define ADD_CONTACT
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "struct.h"
 
+// cria um contato à prtir das infomrações passadas pelo usuário
 Contact form_contact(char name[20], char email[20], char adress[20], char phone[10], char birth_date[11]) {
+
     Contact contact;
+
     strcpy(contact.name, name);
     strcpy(contact.email, email);
     strcpy(contact.adress, adress);    
@@ -19,13 +23,35 @@ Contact form_contact(char name[20], char email[20], char adress[20], char phone[
 
 }
 
+int alphaSort(Contact *array, Contact contact, int size) {
+    
+    if (array == NULL) {
+        return 0;
+    }
 
-int add_contact (Contact contact, Contact array[], int size) {
+    for (int i = 0; i < size; i++)
+    {
 
-    array = (Contact*) realloc(array, (sizeof(Contact))*(size + 1));
+        if (contact.name[0] > array[i].name[0]) {
+            return i;
+        } 
 
-    array[size - 1] = contact;
+        return size;
+    }
 
-    return 0;
+}
+
+void add_contact (Contact contact, Contact *array, int* size, int index) {
+
+    array = (Contact*) realloc(array, (sizeof(Contact))*(*size + 1));
+
+    for (int i = *size; i > index; i--)
+    {
+        array[i] = array[i - 1]; 
+    }
+    
+    array[index] = contact;
+
+    *size++;
 }
 #endif 
