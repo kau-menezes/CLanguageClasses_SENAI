82ad9ba3f6793c2ae0c56da0e38420db8ebfa57d
akauzinha47 <kau.vmenezes@gmail.com>
2024-05-20 17:04:08 -0300

    feat: contact deletion

--- Structs - Agenda Challange/main.c
+++ Structs - Agenda Challange/main.c
@@ -17,10 +17,20 @@ int main() {
     while (1) 
     {
 
-        printf("\n+======================= MENU =======================+\n\n");
-        printf("\n|                                                    |");
-        printf("\n|   Type below the operation you'd like to perform:  |");
-        printf("\n|1. Add new contact\n2. Delete contact\n3. Search by first name\n4. Show data from all contacts\n5. Show name and phone from all contacts\n6. Exit application\n\nR: ");
+        printf("\n+====================== MENU ======================+");
+        printf("\n|                                                  |");
+        printf("\n|             Type below the operation             |");
+        printf("\n|               you'd like to perform.             |");
+        printf("\n|                                                  |");
+        printf("\n|               1. Add new contact                 |");
+        printf("\n|               2. Delete contact                  |");  
+        printf("\n|             3. Search by first name              |");
+        printf("\n|           4. Show data from all contacts         |");
+        printf("\n|     5. Show name and phone from all contacts     |");
+        printf("\n|               6. Exit application                |");
+        printf("\n|                                                  |");
+        printf("\n+==================================================+");
+        printf("\n\nR: ");
         scanf(" %d", &choice);
 
         switch (choice)
@@ -40,6 +50,7 @@ int main() {
             Contact contact = construct_contact(name, phone, adress);
 
             add_sort(&contact_list, contact);
+            contact_list.quantity++;
 
             printf("\nContact added.");
             
@@ -47,35 +58,63 @@ int main() {
         
         case 2:
 
-            printf("\nWorking on that.");
+            printf("\nType below the numbre that corresponds to the contact you'd like to eliminate.\n\nR: ");
+
+            for (int i = 0; i < contact_list.quantity; i++) {
+                printf("\n%d. %s - Phone: %s", i, contact_list.array[i],name, contact_list.array[i],phone);
+            }
+            scanf("%d", &choice);
+
+            for (int i = choice; i < contact_list.quantity; i++) {
+                contact_list.array[i] = contact_list.array[i + 1];
+            }
+
+            contact_list.quantity--;
+
+            printf("\nContact eliminated. >:)\n");
+            break;
+
 
         case 3:
 
-            printf("\nWorking on that.");
+            printf("\nType their first name: ");
+            scanf("%s", name);
+
+            for (int i = 0; i < contact_list.quantity; i++) 
+            {
+                if (strcmp(name, contact_list.array[i].name)) 
+                {
+                    printf("\nName: %s", contact_list.array[i].name);
+                    printf("\nPhone: %s", contact_list.array[i].phone);
+                }
+            }
+                
+            break;
 
         case 4:
 
-            printf("\n\n=== CONTACTS ===");
+            printf("\n\n====================== CONTACTS ======================");
 
-            for (int i = 0; i < contact_list.quantity; i++) {
+            for (int i = 0; i < contact_list.quantity; i++) 
+            {
                 printf("\nName: %s", contact_list.array[i].name);
                 printf("\nPhone: %s", contact_list.array[i].phone);
                 printf("\nAdress: %s", contact_list.array[i].adress);
 
-                printf("\n----------------");
+                printf("\n------------------------------------------------------");
             }
 
             break;
 
         case 5:
 
-            printf("\n\n=== CONTACTS ===");
+            printf("\n\n====================== CONTACTS ======================\n");
 
             for (int i = 0; i < contact_list.quantity; i++) {
                 printf("\nName: %s", contact_list.array[i].name);
                 printf("\nPhone: %s", contact_list.array[i].phone);
 
-                printf("\n----------------");
+                printf("\n------------------------------------------------------");
             }
 
             break;
